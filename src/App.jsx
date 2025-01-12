425b98296a4186fb5d0e7683c276ef7341567424
KT147 <kevin.tammm@gmail.com>
2025-01-12 09:37:07 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Books from "./pages/Books";
 import Avaleht from "./pages/Avaleht";
 import Numbrid from "./pages/Numbrid";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -16,11 +17,15 @@ function App() {
       <Link to= "/numbrid">
       <button>Numbrid</button>
       </Link>
+      <Link to= "/notfound">
+      <button>Not Found</button>
+      </Link>
 
       <Routes>
         <Route path="books" element={<Books />} />
         <Route path="/" element={<Avaleht />} />
         <Route path="/numbrid" element={<Numbrid/>}/>
+        <Route path="/notfound" element={<NotFound/>}/>
       </Routes>
     </>
   );
